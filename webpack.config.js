75f93b30e6c2577ed8a730ef85217fc5f690c19b
xm <dxp157@sohu.com>
2018-12-22 14:13:19 +0800

    配置 babel 并且使用 babelrc 文件管理

--- webpack.config.js
+++ webpack.config.js
@@ -6,4 +6,14 @@ module.exports = {
       path: path.resolve(__dirname, 'build'), // 必须使用绝对地址，输出文件夹
       filename: "bundle.js" // 打包后输出文件的文件名
     }
+    module: {
+      rules: [{
+        // js 文件才使用 babel
+        test: /\.js$/,
+        // 使用哪个 loader
+        use: 'babel-loader',
+        // 不包括路径
+        exclude: /node_modules/
+      }]
+    }
   }
